5927996cc7cbb9653b36d94d6055a7a3e64e8752
Yash Sharma <ys909@snu.edu.in>
2020-04-23 04:41:13 +0530

    forgot password page

---
+++ lib/pages/onboarding_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:aztlan/shared/colors.dart';
+
+class Onboard extends StatefulWidget {
+  @override
+  _OnboardState createState() => _OnboardState();
+}
+
+class _OnboardState extends State<Onboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
