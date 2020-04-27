1bd8f24c3538a0f5a2c0e255b278bbb59da071eb
Yash Sharma <ys909@snu.edu.in>
2020-04-27 07:53:35 +0530

    web_initial

---
+++ lib/extensions/hover_extension.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'dart:html' as html;
+
+extension HoverExtension on Widget{
+  static final appContainer =html.window.document.getElementById('app-container');
+
+}
